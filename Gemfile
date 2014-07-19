dd8409b1e7df17c2851de654113db2f11b979a41
Michael Ohm <michael.k.ohm@gmail.com>
2014-07-18 17:20:50 -0700

    added devise, added user api endpoint

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'spring',        group: :development
 #API gems
 gem 'active_model_serializers'
 
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
-  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+  devise_for :users
 
+  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    	resources :users, :only => [:show]
+    end
   end
 
 end
